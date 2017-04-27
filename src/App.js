ab53b931b44d9c2e5bf105f6525a25a4acf5f5f8
Tyler Davis <tyjdavis@outlook.com>
2017-04-27 16:34:03 -0400

    file structure created. Main shell done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import Album from './Album';
+import Aside from './Aside';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <section className="container">
+            <Header
+
+            />
+            <Main
+
+            />
+        </section>
+      </Router>
     );
   }
 }
---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+
+  render() {
+    return(
+      <section className="row">
+        <div className="col s12 m4 l4">Album 1</div>
+        <div className="col s12 m4 l4">Album 2</div>
+        <div className="col s12 m4 l4">Album 3</div>
+        <div className="col s12 m4 l4">Album 4</div>
+        <div className="col s12 m4 l4">Album 5</div>
+        <div className="col s12 m4 l4">Album 6</div>
+      </section>
+      )
+    }
+}
+
+export default Main
